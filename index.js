89ac14d165c5ab710b6abe7bce3e7d3997f6867e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-12 01:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, array.length - 1);
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  return kitten.slice(1);
+  return kittens.slice(1);
 }
